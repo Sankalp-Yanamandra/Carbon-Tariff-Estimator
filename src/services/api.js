dc57d9f67849033d16a639846dd6760caf8f1005
shivan-987 <174676664+shivan-987@users.noreply.github.com>
2026-06-19 09:41:01 +0530

    preparing frontend data for deployment on Netlify

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 /*
-    instead of writing "http://localhost:3000" in every file, 
+    instead of writing "http://localhost:3000" or "https://logistics-api-k0wx.onrender.com/" in every file, 
     created 1 axios instance that always points to our backend : json server
 */
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://logistics-api-k0wx.onrender.com/"
 });
 
 export default api;
